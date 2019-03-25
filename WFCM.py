860d526fb5f7c0528f1a2ad5f270e9bcbc4c5a55
lee <you@example.com>
2019-03-25 21:11:04 +0800

    下班了

--- WFCM.py
+++ WFCM.py
@@ -1,25 +1,30 @@
 import numpy as np
 from sklearn import datasets
 import matplotlib.pyplot as plt
+import pandas as pd;
 from mpl_toolkits.mplot3d import Axes3D
 import tensorflow as tf
 import warnings
 
-
+def loadData(iris):
+    X=iris.data
+    y=iris.target
+    return X,y
 # 生产随机中心点
 def getRamdonCenter(k, x_data):
     # mininum = np.min(x_data)
     # maxinum = np.max(x_data)
     maxList = np.argmax(x_data, 0)
-    maxList = list(x_data[item][index] for index, item in enumerate(maxList))
-    minList = np.argmin(x_data, 0)
-    minList = list(x_data[item][index] for index, item in enumerate(minList))
-    res = []
-    for i in range(k):
-        # for mi, ma in zip(np.array(minList), np.array(maxList)):
-            # print(mi, ma)
-        res.append(list(np.random.uniform(low=mi, high=ma) for mi, ma in zip(np.array(minList), np.array(maxList))))
-    return np.array(res)
+    maxList = tf.map_fn(elems= maxList, fn=lambda index: 1) # x_data[maxList[index]][index]
+    return maxList
+    # minList = np.argmin(x_data, 0)
+    # minList = tf.map_fn(elems=minList, fn=lambda index: x_data[minList[index]][index])
+    # res = []
+    # for i in range(k):
+    #     # for mi, ma in zip(np.array(minList), np.array(maxList)):
+    #         # print(mi, ma)
+    #     res.append(list(np.random.uniform(low=mi, high=ma) for mi, ma in zip(np.array(minList), np.array(maxList))))
+    # return np.array(res)
 
 
 def formate(item, mininum, maxinum):
@@ -45,13 +50,32 @@ def distance(x1_list, x2_list):
     sum = sum ** 0.5
     return sum
 def get_membership(x_data):
-    membership = np.random.uniform(len(x_data))
+    membership = np.random.random(size=(3, len(x_data)))
     return membership
+
+def draw(data):
+    # data = pd.DataFrame(data)
+    fig = plt.figure() 
+    ax = Axes3D(fig) 
+    ax.scatter(data[:, 0], data[:, 2], data[:, 1])
+
+def train(x_data):
+    with tf.Session() as sess:
+        x_data_tf = tf.placeholder(shape=[None, 4], dtype=tf.float32)
+        centerList = getRamdonCenter(3, x_data_tf)
+        # distance_list = list(list(distance(x, i) for x in x_data) for i in centerList)
+        # loss = tf.sum(tf.multiply(get_membership(x_data_tf), distance_list))
+        # train_step = my_opt.minimize(loss)
+        # 训练开始
+        init = tf.global_variables_initializer()
+        sess.run(init)
+        print(sess.run(centerList, feed_dict={x_data_tf: x_data}))
+        # sess.run(centerList, feed_dict={x_data_tf: x_data})
 # FCM
 if __name__ == "__main__":
-    X_data,y = loadData(datasets.load_iris());
-    X_data = toMatrix(X_data)
-    centerList = getRamdonCenter(3, X_data)
-    distance_list = list(list(distance(x, i) for x in X_data) for i in centerList)
-    np.random.rand(4,3)
-    print(get_membership(X_data))
+    x_data,y = loadData(datasets.load_iris());
+    draw(x_data)
+    x_data = toMatrix(x_data)
+    # loss = np.sum(np.multiply(get_membership(x_data), distance_list))
+    train(x_data)
+        
